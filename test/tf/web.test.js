1f2baa9be76eb644b9c7d3f69b5ae477ea4f9fa6
Evgeny V. Generalov <e.generalov@gmail.com>
2019-06-30 08:58:50 +0300

    fix: open in browser path escaping

---
+++ test/tf/web.test.js
@@ -0,0 +1,14 @@
+const assert = require("assert")
+const { buildVersionControlUrl } = require("../../lib/tf/web")
+
+describe("tf.web.buildVersionControlUrl", function() {
+  it("should build url", function() {
+    const collection = "http://server/tfs"
+    const serverPath = "$/path/to/main.js"
+
+    assert.equal(
+      buildVersionControlUrl(collection, serverPath),
+      "http://server/tfs/_versionControl#path=%24%2Fpath%2Fto%2Fmain.js"
+    )
+  })
+})
